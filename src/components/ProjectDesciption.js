fadaac7a8b1303ee09608db479707932a0bef603
ParichayPai <parichaypai393@gmail.com>
2020-11-05 12:54:51 +0530

    changes

---
+++ src/components/ProjectDesciption.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function ProjectDescription(props) {
+  return <p>{props["data-ns-test"]}</p>;
+}
+
+export default ProjectDescription;
---
+++ src/components/ProjectName.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function ProjectName(props) {
+  return <h1>{props["data-ns-test"]}</h1>;
+}
+
+export default ProjectName;
